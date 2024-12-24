be0fc1a9347a0ce5e4b95c61e2d7577de49b7203
JakeSteve <jake.fernandezbrizuela@outlook.com>
2024-12-23 21:12:44 -0500

    Fixing links redirection

--- src/components/nav/HomeNav.tsx
+++ src/components/nav/HomeNav.tsx
@@ -9,7 +9,7 @@ const HomeNav = () => {
       >Login</Link>
       <Link
         className='bg-lime-500 text-slate-800 rounded-lg p-2 uppercase font-black text-xs cursor-pointer'
-        to={'/auth/login'}
+        to={'/auth/register'}
       >Register</Link>
     </>
   )
